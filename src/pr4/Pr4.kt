4189ffef8e5f7026ddd7a638438944f04c420088
Mohammadreza khahani <mr.khahani@gmail.com>
2021-10-13 14:10:51 +0330

    Description for the second week practice included.

--- src/pr4/Pr4.kt
+++ src/pr4/Pr4.kt
@@ -1,9 +1,11 @@
 package pr4
-
+//Implement the function that checks whether a string is a valid identifier.
+// A valid identifier is a non-empty string that starts with a letter or
+// underscore and consists of only letters, digits and underscores.
 object Pr4 {
     @JvmStatic
     fun main(args: Array<String>) {
-        runTests();
+        runTests()
     }
 
     private fun runTests() {
